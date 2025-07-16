d73072f98b156beade43f1380646d6e55fd9e76a
Shivani151805 <sumaninfo97@gmail.com>
2025-07-17 00:41:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getDOMLevel() {
+    let element = document.getElementById("level");
+    let level = 0;
+
+    
+    while (element) {
+        level++;
+        element = element.parentElement;
+    }
+
+    console.log("DOM Level:", level);
+    alert("DOM Level: " + level);
